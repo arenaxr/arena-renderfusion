422f94392db37a6941d4bd23890e72e9a6a8704a
mathiast69 <thomasmathias270@gmail.com>
2023-01-28 10:01:28 -0500

    Style Changes
    Former-commit-id: 76fcdc10bcb51d10c8bfa0d98d3300fab558e810

--- Hybrid-Application-Launcher/test2.py
+++
@@ -1,7 +0,0 @@
-from arena import *
-
-def testCallBack(client, userdata,msg):
-    pass
-scene = Scene(host="arena-dev1.conix.io", scene="example")
-
-scene.message_callback_add("realm/g/a/hybrid_rendering/client/hi/#", testCallBack)
