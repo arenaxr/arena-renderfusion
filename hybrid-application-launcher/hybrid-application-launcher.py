7332f575e8603f1688d5a11a19d3fab9899d60a8
Edward <lued2018@gmail.com>
2023-02-06 22:03:51 -0500

    style fixes
    Former-commit-id: 48ac91d25ed10b2e808b4c722ce57321272dde88

--- hybrid-application-launcher/hybrid-application-launcher.py
+++ hybrid-application-launcher/hybrid-application-launcher.py
@@ -16,9 +16,9 @@ clientDict = dict()
 SceneDict = dict()
 remoteRender = dict()
 executableQueue = []
+
 def openExecutable(data):
-   
-    if(platform == "linux" or platform == "linux2"):
+    if (platform == "linux" or platform == "linux2"):
         fileName = ["./arena-hybrid.x86_64",data]  
     else:
         fileName = ["open","HydribExe.app","--args",data]
@@ -29,49 +29,45 @@ def setupHybrid():
     id = str(uuid.uuid1())
     executableQueue.append(id)
     openExecutable(id)
-    return
 
 def onDisconnect(client, userdata,msg):
     request = json.loads((msg.payload.decode("utf-8")))
     print(request)
-    if("data" not in request):
+    if ("data" not in request):
         logging.warning("Request does not have namespace info, ignoring")
         return    
     data = request['data']
     id = request["id"]
-    if(data in clientDict):
-        if(id in clientDict[data]):
+    if (data in clientDict):
+        if (id in clientDict[data]):
             clientDict[data].remove(id)
 
-        if(not clientDict[data]):
+        if (not clientDict[data]):
             closeExecutable(data)
 
 def renderStatus(client, userdata, msg):
     request = json.loads((msg.payload.decode("utf-8")))
     print(request)
-    if("data" not in request and "remoteRendered" not in request["data"]):
+    if ("data" not in request and "remoteRendered" not in request["data"]):
         logging.warning("Request does not have namespace info, ignoring")
         return
     renderStatus= request["data"]["remoteRendered"]
     data = request["data"]["data"]
-    if(data not in remoteRender):
+    if (data not in remoteRender):
         remoteRender[data] = set()
         remoteRender[data].add(msg)
     else:
         remoteRender[data].add(msg)
 
-    return
 def sendRenderStatus(client, userdata,msg):
     request = json.loads((msg.payload.decode("utf-8")))
     print(request)
-    return 
-    
+
 def onConnect(client, userdata, msg):
-    
     request = json.loads((msg.payload.decode("utf-8")))
     request['type'] = "HAL"
     #print(request)
-    if("data" not in request):
+    if ("data" not in request):
         logging.warning("Request does not have namespace info, ignoring")
         return
     connectData = request['data']
@@ -79,26 +75,20 @@ def onConnect(client, userdata, msg):
     data = connectData['namespacedScene']
     
 
-    if("namespacedScene"not in connectData):
+    if ("namespacedScene"not in connectData):
         logging.warning("Request does not have namespace info, ignoring")
         return
     data = connectData['namespacedScene']
     id = request["id"]
-    if(data in clientDict):
+    if (data in clientDict):
         clientDict[data].add(id)
     else:
-            topic = HAL_CONNECT + executableQueue.pop()
-            print(topic)
-            scene.mqttc.publish(topic,json.dumps(request, ensure_ascii=False).encode('utf-8'))
-            clientDict[data] = set()
-            clientDict[data].add(id)
+        topic = HAL_CONNECT + executableQueue.pop()
+        print(topic)
+        scene.mqttc.publish(topic,json.dumps(request, ensure_ascii=False).encode('utf-8'))
+        clientDict[data] = set()
+        clientDict[data].add(id)
 
-    return
-
-
-
-    
-    
 def closeExecutable(data):
     print(data)
     Popen = SceneDict[data]
@@ -110,7 +100,6 @@ def closeExecutable(data):
         logging.warning("Process Does not exist")
     SceneDict.pop(data)
     clientDict.pop(data)
-    pass
 
 setupHybrid()
 
