4524aa32f1ed86c2237a2373b3e4d3f8ff9d2db0
Edward <lued2018@gmail.com>
2023-02-11 22:06:51 -0500

    works in hdrp

---
+++ ARENAHybridRenderingHD/RGBDepthShaderHD.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using System;
+using ArenaUnity;
+
+namespace ArenaUnity.HybridRendering
+{
+    [Serializable, VolumeComponentMenu("Post-processing/Custom/RGBDepthShaderHD")]
+    public sealed class RGBDepthShaderHD : CustomPostProcessVolumeComponent, IPostProcessComponent
+    {
+        private Material m_Material;
+
+        private int m_HasDualCameras = 0;
+
+        public bool IsActive() => m_Material != null;
+
+        public override CustomPostProcessInjectionPoint injectionPoint => CustomPostProcessInjectionPoint.AfterPostProcess;
+
+        public override void Setup()
+        {
+            if (Shader.Find("Hidden/RGBDepthShaderHD") != null)
+                m_Material = new Material(Shader.Find("Hidden/RGBDepthShaderHD"));
+        }
+
+        public override void Render(CommandBuffer cmd, HDCamera camera, RTHandle source, RTHandle destination)
+        {
+            if (m_Material == null)
+                return;
+
+            var hybridCamera = camera.camera.gameObject.GetComponent<HybridCamera>();
+            if (hybridCamera)
+            {
+                m_HasDualCameras = (hybridCamera.isDualCamera) ? 1 : 0;
+            }
+            m_Material.SetTexture("_MainTex", source);
+            m_Material.SetInt("_DualCameras", m_HasDualCameras);
+
+            HDUtils.DrawFullScreen(cmd, m_Material, destination);
+        }
+
+        public override void Cleanup() => CoreUtils.Destroy(m_Material);
+    }
+}
